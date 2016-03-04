fd008fc0895109a56a71c0ca8ca002703baa4f30
Elliott Minns <elliott.minns@me.com>
2016-03-04 19:32:50 +0000

    Updated for linux

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
 import PackageDescription
 
+#if os(Linux)
+let uvModule = "https://github.com/elliottminns/uv-module-linux.git"
+#else
+let uvModule = "https://github.com/elliottminns/uv-module.git"
+#endif
+
 let package = Package(
     name: "Echo",
     dependencies: [
-        .Package(url: "https://github.com/elliottminns/uv-module.git",
+        .Package(url: uvModule
                  majorVersion: 0)
     ]
 )
