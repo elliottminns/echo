1589121daee91d834d4d61617a454514f230537c
Elliott Minns <elliott.minns@me.com>
2016-03-23 13:56:06 +0000

    Fixed ordering

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
 import PackageDescription
 
 #if os(Linux)
-let libuvURL = "https://github.com/elliottminns/libuv-osx"
-#else
 let libuvURL = "https://github.com/elliottminns/libuv-linux"
+#else
+let libuvURL = "https://github.com/elliottminns/libuv-osx"
 #endif
 
 let package = Package(
