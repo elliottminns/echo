bd7c9ae25c1ba1b0ef998f4fed088ad9b9e770a9
Elliott Minns <elliott.minns@me.com>
2016-03-23 18:30:26 +0000

    Added back in Run loop

--- Sources/Echo.swift
+++ Sources/Echo.swift
@@ -17,19 +17,19 @@ public class Echo {
     func begin() {
         if !running {
             running = true
-            //#if os(Linux)
+            #if os(Linux)
             mainQueue.run()
-            //#else
-            //let resolution = 1.0;
-            //var isRunning: Bool = false
-            //repeat {
-                //let next = NSDate(timeIntervalSinceNow: resolution)
-                //let loop = NSRunLoop.mainRunLoop()
+            #else
+            let resolution = 1.0;
+            var isRunning: Bool = false
+            repeat {
+                let next = NSDate(timeIntervalSinceNow: resolution)
+                let loop = NSRunLoop.main()
 
-                //isRunning = loop.runMode(NSDefaultRunLoopMode, 
-                //                         beforeDate: next)
-            //} while (running && isRunning)
-            //#endif
+                isRunning = loop.runMode(NSDefaultRunLoopMode, 
+                                         before: next)
+            } while (running && isRunning)
+            #endif
 
         }
     }
