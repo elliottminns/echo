0959da242eea505ecb04625d9303c3dd145eab9a
Elliott Minns <elliott.minns@me.com>
2016-03-23 13:55:07 +0000

    Linux bindings

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
 import PackageDescription
 
+#if os(Linux)
+let libuvURL = "https://github.com/elliottminns/libuv-osx"
+#else
+let libuvURL = "https://github.com/elliottminns/libuv-linux"
+#endif
+
 let package = Package(
     name: "Echo",
     dependencies: [
-        .Package(url: "https://github.com/elliottminns/libuv-osx",
+        .Package(url: libuvURL,
                  majorVersion: 0)
     ]
 )
